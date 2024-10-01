9765da061984807fd17dd8dfc4d7c9477cd649ce
Mayukha <ivaturimayukha@gmail.com>
2024-10-01 13:30:00 +0530

    Adding previous data

--- seeds/index.js
+++ seeds/index.js
@@ -1,9 +1,11 @@
+require(dotenv).config();
 const mongoose = require('mongoose');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
